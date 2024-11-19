0825beb953d328a0c2f6932b8c700575ce9262f8
mikromajor <robocza.sowa@yandex.com>
2024-11-19 21:57:52 +0100

    create bot, whose send back the messages

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const BotApi = require("node-telegram-bot-api");
+
 const token =
-  "8158243553:AAGminTl7gse4fbQouRw3-TVgavN53-xJ2E";
+  "7458656190:AAEuiDB5Rh78hsUcACRJxEUM44tDYJUQhLo";
+
+const bot = new BotApi(token, { polling: true });
+
+bot.on("message", (msg) => {
+  console.log("", msg);
+  const chatId = msg.chat.id;
+  const message = msg.text;
+  bot.sendMessage(
+    chatId,
+    "T-1000 send the message: " + message
+  );
+});
+// console.log("idex.js started", bot);
