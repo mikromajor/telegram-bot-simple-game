68af4ce86c9d2c05a45e7d93ddd27f21998398b1
mikromajor <robocza.sowa@yandex.com>
2024-11-23 09:17:57 +0100

    add msgStructure

--- index.js
+++ index.js
@@ -5,13 +5,49 @@ const token =
 
 const bot = new BotApi(token, { polling: true });
 
-bot.on("message", (msg) => {
+bot.setMyCommands([
+  {
+    command: "/go",
+    description: "Start communicating with bot",
+  },
+  { command: "/inf", description: "information" },
+  { command: "/help", description: "help" },
+]);
+
+bot.on("message", async (msg) => {
   console.log("", msg);
   const chatId = msg.chat.id;
   const message = msg.text;
-  bot.sendMessage(
+  await bot.sendSticker(
     chatId,
-    "T-1000 send the message: " + message
+    "https://cdn.tlgrm.ru/stickers/88d/9dc/88d9dc34-0a9d-4de5-b94e-3f34adacece5/2.webp"
   );
+  if (message === "/start") {
+    await bot.sendMessage(
+      chatId,
+      "Welcome, I'm bot T-1000: " + message
+    );
+  }
 });
-// console.log("idex.js started", bot);
+
+const msgStructure = {
+  message_id: 10,
+  from: {
+    id: 852623084,
+    is_bot: false,
+    first_name: "Alex",
+    last_name: "Solianyk",
+    username: "AlexSolianyk",
+    language_code: "uk",
+  },
+  chat: {
+    id: 852623084,
+    first_name: "Alex",
+    last_name: "Solianyk",
+    username: "AlexSolianyk",
+    type: "private",
+  },
+  date: 1732126856,
+  text: "/start",
+  entities: [{ offset: 0, length: 6, type: "bot_command" }],
+};
